158e0a3ea3e34b39245fd1ee0b8b652d3c61bbb0
Vaibhavdalvi1 <vaibhav.dalvi@geexu.in>
2022-10-28 19:11:27 +0530

    new commit

---
+++ Recursions practice examples/setTimeout.js
@@ -0,0 +1,16 @@
+function showTime() {
+
+    // return new date and time
+    let dateTime= new Date();
+
+    // returns the current local time
+    let time = dateTime.toLocaleTimeString();
+
+    console.log(time)
+
+    // display the time after 3 seconds
+     setTimeout(showTime, 3000);
+}
+
+// calling the function
+showTime();
